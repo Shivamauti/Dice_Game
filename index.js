fa6f1bdca1beb77789941a11ac71ec9a82e0fd48
Shivamauti <96620360+Shivamauti@users.noreply.github.com>
2022-08-11 23:46:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "" + randomDiceImage; //images/dice1.png - images/dice6.png
+// var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "dice" + randomNumber1 + ".png";
+// var randomImageSource = "" + randomDiceImage; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
